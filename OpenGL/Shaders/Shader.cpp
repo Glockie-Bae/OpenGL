251ea1d858947752761a89bf9eb3a88a6887e625
Glockie-Bae <2358750802@qq.com>
2024-09-02 15:25:24 +0800

    texture

--- OpenGL/Shaders/Shader.cpp
+++ OpenGL/Shaders/Shader.cpp
@@ -1,121 +1,3 @@
-<<<<<<< HEAD
-#include "Shader.h"
-
-Shader::Shader(const char* vertexPath, const char* fragmentPath)
-{
-	std::string vertexCode;
-	std::string fragmentCode;
-	std::ifstream vShaderFile;
-	std::ifstream fShaderFile;
-	// 保证ifstream对象可以抛出异常：
-	vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-	fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
-
-	try
-	{
-		// 打开文件
-		vShaderFile.open(vertexPath);
-		fShaderFile.open(fragmentPath);
-		std::stringstream vShaderStream, fShaderStream;
-		// 读取文件的缓冲内容到数据流
-		vShaderStream << vShaderFile.rdbuf();
-		fShaderStream << fShaderFile.rdbuf();
-		// 关闭文件处理器
-		vShaderFile.close();
-		fShaderFile.close();
-		// 转换数据流到string
-		vertexCode = vShaderStream.str();
-		fragmentCode = fShaderStream.str();
-	}
-	catch (std::ifstream::failure e)
-	{
-		std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
-
-	}
-
-	const char* vShaderCode = vertexCode.c_str();
-	const char* fShaderCode = fragmentCode.c_str();
-
-	// 编译shaders
-	unsigned int vertex, fragment;
-	vertex = glCreateShader(GL_VERTEX_SHADER);
-	fragment = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(vertex, 1, &vShaderCode, NULL);
-	glShaderSource(fragment, 1, &fShaderCode, NULL);
-	glCompileShader(vertex);
-	checkCompileErrors(vertex, "VERTEX");
-	glCompileShader(fragment);
-	checkCompileErrors(vertex, "VERTEX");
-
-	// shader 程序
-	m_ShaderProgram = glCreateProgram();
-	glAttachShader(m_ShaderProgram, vertex);
-	glAttachShader(m_ShaderProgram, fragment);
-	glLinkProgram(m_ShaderProgram);
-
-
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
-}
-
-void Shader::UseProgram()
-{
-	glUseProgram(m_ShaderProgram);
-}
-
-void Shader::SetBool(const std::string& name, bool value) const
-{
-	glUniform1i(glGetUniformLocation(m_ShaderProgram, name.c_str()), (int)value);
-}
-
-void Shader::SetInt(const std::string& name, int value) const
-{
-	glUniform1i(glGetUniformLocation(m_ShaderProgram, name.c_str()), value);
-}
-
-void Shader::SetFloat(const std::string& name, float value) const
-{
-	glUniform1f(glGetUniformLocation(m_ShaderProgram, name.c_str()), value);
-}
-
-void Shader::Set4Float(const std::string& name, float v0, float v1, float v2, float v3) const
-{
-	glUniform4f(glGetUniformLocation(m_ShaderProgram, name.c_str()), v0, v1, v2, v3);
-}
-
-void Shader::DeleteProgram()
-{
-	glDeleteProgram(m_ShaderProgram);
-}
-
-
-
-
-void Shader::checkCompileErrors(unsigned int shader, std::string type)
-{
-	// 判断是否vertex shader编译成功
-	int  success;
-	char infoLog[512];
-	if (type == "PROGRAM") {
-		// check for linking errors
-			glGetProgramiv(shader, GL_LINK_STATUS, &success);
-		if (!success) {
-			glGetProgramInfoLog(shader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-		}
-	}
-	else {
-		glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-
-		if (!success)
-		{
-			glGetShaderInfoLog(shader, 512, NULL, infoLog);
-			std::cout << "ERROR::SHADER::" << type << "::COMPILATION_FAILED\n" << infoLog << std::endl;
-		}
-	}
-	
-}
-=======
 #include "Shader.h"
 
 
@@ -223,7 +105,7 @@ void Shader::checkCompileErrors(unsigned int shader, std::string type)
 	char infoLog[512];
 	if (type == "PROGRAM") {
 		// check for linking errors
-			glGetProgramiv(shader, GL_LINK_STATUS, &success);
+		glGetProgramiv(shader, GL_LINK_STATUS, &success);
 		if (!success) {
 			glGetProgramInfoLog(shader, 512, NULL, infoLog);
 			std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
@@ -238,6 +120,5 @@ void Shader::checkCompileErrors(unsigned int shader, std::string type)
 			std::cout << "ERROR::SHADER::" << type << "::COMPILATION_FAILED\n" << infoLog << std::endl;
 		}
 	}
-	
+
 }
->>>>>>> 473ceed465fa42af16aaca5e8ac30b611477323a
--- OpenGL/Shaders/Shader.h
+++ OpenGL/Shaders/Shader.h
@@ -1,36 +1,3 @@
-<<<<<<< HEAD
-#pragma once
-
-#include <glad/glad.h>; // 包含glad来获取所有的必须OpenGL头文件
-
-#include <string>
-#include <fstream>
-#include <sstream>
-#include <iostream>
-
-
-class Shader
-{
-public:
-
-    // 构造器读取并构建着色器
-    Shader(const char* vertexPath, const char* fragmentPath);
-    // 使用/激活程序
-    void UseProgram();
-    // uniform工具函数
-    void SetBool(const std::string& name, bool value) const;
-    void SetInt(const std::string& name, int value) const;
-    void SetFloat(const std::string& name, float value) const;
-	void Set4Float(const std::string& name, float v0, float v1, float v2, float v3) const;
-    void DeleteProgram();
-
-
-private:
-	void checkCompileErrors(unsigned int shader, std::string type);
-    unsigned int m_ShaderProgram;
-};
-
-=======
 #pragma once
 
 #include <glad/glad.h>; // 包含glad来获取所有的必须OpenGL头文件
@@ -68,5 +35,3 @@ private:
 	void checkCompileErrors(unsigned int shader, std::string type);
     unsigned int m_ShaderProgram;
 };
-
->>>>>>> 473ceed465fa42af16aaca5e8ac30b611477323a
